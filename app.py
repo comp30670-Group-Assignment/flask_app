1e9cb52a56f15c5c75dc2c41dc921abeca6ca825
Aalasa Fidler <aalasa.fidler@ucdconnect.ie>
2018-03-11 18:57:11 +0000

    Trial push to github of starter flask app.

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from data import Articles
+
+app = Flask(__name__)
+
+Articles = Articles()
+
+#placeholder for current file
+@app.route('/')
+def index():
+# Normally we return a template, not a string.
+	return render_template('home.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+	return render_template('articles.html', articles = Articles)
+
+#string.id is a dynamic value
+@app.route('/article/<string:id>/')
+def article(id):
+	return render_template('article.html', id=id)
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+                        'id' :1,
+                        'title':'Article One',
+                        'body':'lorem',
+                        'author':'Aalasa Fidler',
+                        'create_Date':'11/March-2018'
+                        },
+                {
+                        'id' :2,
+                        'title':'Article One',
+                        'body':'lorem',
+                        'author':'A F',
+                        'create_Date':'11/March-2018'
+                        },
+                {
+                        'id' :3,
+                        'title':'Article One',
+                        'body':'lorem',
+                        'author':'A Fidler',
+                        'create_Date':'11/March-2018'
+                        }
+                ]
+	return articles
