78b59d08111b8cbe268c585e46e65db680c026d4
Hugh <hug_hob@hotmail.com>
2018-03-29 09:12:57 +0100

    edits

--- extractor/extractor_test.py
+++ extractor/extractor_test.py
@@ -8,17 +8,17 @@ new_conex.getLatAndLong()
 
 print(new_conex.json)
 
-new_conex.avg_available_bike()
+#new_conex.avg_available_bike()
 
-print(new_conex.avg_available_bike_dict)
+#print(new_conex.avg_available_bike_dict)
 
-new_conex.avg_available_stand()
+#new_conex.avg_available_stand()
 
-print(new_conex.avg_available_stand_dict)
+#print(new_conex.avg_available_stand_dict)
 
-print(new_conex.avg_available_bike_dict['BARROW STREET'][0])
+#print(new_conex.avg_available_bike_dict['BARROW STREET'][0])
 
-print(new_conex.avg_available_stand_dict['BARROW STREET'][0])
+#print(new_conex.avg_available_stand_dict['BARROW STREET'][0])
 
 
 
